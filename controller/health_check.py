523f7723b03956d17b7908139ad2a5e677be9d68
kunal.chau <kunal.chau@media.net>
2021-02-16 01:02:02 +0530

    Adds metric log

--- controller/health_check.py
+++ controller/health_check.py
@@ -1,6 +1,11 @@
 from http import HTTPStatus
+from log.logger import get_logger
+from log.metriclog import metric_log
 
 
+@metric_log(key="health-check")
 def health_check():
     """ Controller for health check status """
+    logger = get_logger()
+    logger.info("status", "Application is up and running")
     return "Application is up and running", HTTPStatus.OK
